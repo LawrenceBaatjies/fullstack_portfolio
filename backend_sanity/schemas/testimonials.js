10156cbf47e680cc32a89ae063c5f52da62585ec
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-03-31 14:36:33 +0200

    created testimonial schema

---
+++ backend_sanity/schemas/testimonials.js
@@ -0,0 +1,31 @@
+export default {
+    name: 'testimonials',
+    title: 'Testimonials',
+    type: 'document',
+    fields:[
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'company',
+            title: 'Company',
+            type: 'string'
+        },
+        {
+            name: 'imgurl',
+            title: 'ImgURL',
+            type: 'image',
+            options: {
+                hostspot: true,
+            }
+        },
+        {
+            name: 'feedback',
+            title: 'Feedback',
+            type: 'string'
+        },
+        
+    ],
+}
