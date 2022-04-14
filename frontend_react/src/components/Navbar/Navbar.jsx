21f317da8fabde6a49d3b771e42eb1a6c7d944ea
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-04-15 00:02:53 +0200

    Update Navbar.jsx

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './Navbar.scss';
+import { images } from '../../constants'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+   <nav className="app__navbar">
+     <div className="app__navbar-log">
+       <img src={images.logo} alt="log" />
+     </div>
+     <ul className="app__navbar-links">
+       {['home', 'about', 'work', 'skills','contact'].map((item) => (
+         <li className="app__flex p-text " key={`link-${item}`}>
+           <div />
+           <a href={`#${item}`}>{item}</a>
+         </li>
+       ))}
+     </ul>
+   </nav>
   )
 }
 
