7f1e31ab702f9de6d6d7688449fc60ad22ac8ba8
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-04-14 22:15:22 +0200

    Update App.js

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
-
+import { 
+  About,
+  Footer,
+  Header,
+  Skills,
+  Testimonial,
+  Work } from './container';
+// import index.js from '/.components/header';
+// import { Navbar } from './components';
 
 const App = () => {
   return (
-    <h1>React </h1>
+   <div className="app">
+   <Header />
+   <About />
+   <Work />
+   <Skills />
+   <Testimonial />
+   <Footer />
+   </div>
   );
 }
 
