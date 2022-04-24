36b0459bb66578225a9935768f51e0d1e01c9f61
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-04-24 16:47:35 +0200

    Update About.jsx

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import  { motion } from 'framer-motion';
+
 import './About.scss';
 
+const about = [
+  { title: 'Full Stack P.R.E.N Web Dev', description: 'I am a passionate web developer.', imgUrl: '' },
+  { title: 'Web Design', description: 'I create simple yet elegant websites.', imgUrl: '' },
+  { title: 'UI/UX', description: 'I create beautiful UI/UX designs.', imgUrl: '' },
+  { title: ' Database/Back-end', description: 'I create secure and encrypted databases', imgUrl: '' },
+  { title: 'Ethical Hacker', description: 'Cyber Security & Web Penetration Tester.', imgUrl: '' }
+];
 const About = () => {
   return (
-    <div>About</div>
+    <>
+
+      <h2 classNAme="head-text">
+        I Know that...
+        <span>Good Designs</span>
+        <br />
+        Means...
+        <span>Good Business</span>
+      </h2>
+      <div className="app__profiles">
+
+      </div>
+    </>
   )
 }
 
