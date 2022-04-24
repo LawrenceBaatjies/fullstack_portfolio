5498df897300392491fc232b9c7e40db17227680
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-04-24 16:02:35 +0200

    Update Navbar.jsx

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
    <nav className="app__navbar">
      <div className="app__navbar-log">
-       <img src={images.logo} alt="log" />  
+       <img  className="app__logo" src={images.logo} alt="log" />  
      </div>
      <ul className="app__navbar-links">
        {['home', 'about', 'work', 'skills','contact'].map((item) => (
