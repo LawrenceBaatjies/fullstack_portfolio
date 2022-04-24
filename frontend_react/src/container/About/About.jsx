726da86b9018d9e228a61b4e11d6ee3ebeddfe37
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-04-24 17:17:06 +0200

    Update About.jsx

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import  { motion } from 'framer-motion';
-
+import { images } from '../../constants'
 import './About.scss';
 
 const about = [
-  { title: 'Full Stack P.R.E.N Web Dev', description: 'I am a passionate web developer.', imgUrl: '' },
-  { title: 'Web Design', description: 'I create simple yet elegant websites.', imgUrl: '' },
-  { title: 'UI/UX', description: 'I create beautiful UI/UX designs.', imgUrl: '' },
-  { title: ' Database/Back-end', description: 'I create secure and encrypted databases', imgUrl: '' },
+  { title: 'Full Stack P.R.E.N Web Dev', description: 'I am a passionate web developer.', imgUrl: 'about01' },
+  { title: 'Web Design', description: 'I create simple yet elegant websites.', imgUrl: 'about02' },
+  { title: 'UI/UX', description: 'I create beautiful UI/UX designs.', imgUrl: 'about03' },
+  { title: ' Database/Back-end', description: 'I create secure and encrypted databases', imgUrl: 'about04' },
   { title: 'Ethical Hacker', description: 'Cyber Security & Web Penetration Tester.', imgUrl: '' }
 ];
 const About = () => {
@@ -22,7 +22,19 @@ const About = () => {
         <span>Good Business</span>
       </h2>
       <div className="app__profiles">
-
+        {about.map((about, index) => (
+          <motion.div
+          whileInView={{ opacity: 1} }
+          whileHover={{ scale: 1.1 }}
+          transition={{ duration: 0.5, type: 'tween' }}
+          className="app__profile-item"
+          key={about.title + index}
+          >
+      <img src={about.imgUrl} alt={about.title} />
+      <h2 className="bold-text" style={{ marginTop: 20}}>{about.title}</h2>
+      <p className="p-text" style={{ marginTop: 10}}>{about.description}</p>
+          </motion.div>
+        ))}
       </div>
     </>
   )
