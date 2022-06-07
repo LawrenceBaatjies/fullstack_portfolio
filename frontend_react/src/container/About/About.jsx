9bebca09b6cd7391590d43bab04849e468e94e4f
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-06-08 01:15:03 +0200

    Update About.jsx

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,25 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import  { motion } from 'framer-motion';
-import { images } from '../../constants'
+// import { images } from '../../constants'
 import './About.scss';
+import { urlFor, client } from '../../client';
 
-const about = [
-  { title: 'Full Stack P.R.E.N Web Dev', description: 'I am a passionate web developer.', imgUrl: images.about01 },
-  { title: 'Web Design', description: 'I create simple yet elegant websites.', imgUrl: images.about02  },
-  { title: 'UI/UX', description: 'I create beautiful UI/UX designs.', imgUrl: images.about03  },
-  { title: ' Database/Back-end', description: 'I create secure and encrypted databases', imgUrl: images.about04 },
-  { title: 'SEO Specialist', description: 'Gain Organic Ranking through SEO', imgUrl: images.about01 },
-  { title: 'Ethical Hacker', description: 'Cyber Security & Web Penetration Testing.', imgUrl: images.about02 }
-];
+// const abouts = [
+//   { title: 'Full Stack P.R.E.N Web Dev', description: 'I am a passionate web developer.', imgUrl: images.about01 },
+//   { title: 'Web Design', description: 'I create simple yet elegant websites.', imgUrl: images.about02  },
+//   { title: 'UI/UX', description: 'I create beautiful UI/UX designs.', imgUrl: images.about03  },
+//   { title: ' Database/Back-end', description: 'I create secure and encrypted databases', imgUrl: images.about04 },
+//   { title: 'SEO Specialist', description: 'Gain Organic Ranking through SEO', imgUrl: images.about01 },
+//   { title: 'Ethical Hacker', description: 'Cyber Security & Web Penetration Testing.', imgUrl: images.about02 }
+// ];
+
 const About = () => {
-  return (
-    <>
 
+  const [abouts, setAbouts] = useState([])
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+      .then((data) => setAbouts(data))
+  }, []);
+
+
+  return (
+
+    <>
       <h2 className="head-text">
         I Know that <span> Good Quality Apps</span><br /> Means <span>Good Business.</span>
       </h2>
       <div className="app__profiles">
-        {about.map((about, index) => (
+        {abouts.map((about, index) => (
           <motion.div
           whileInView={{ opacity: 1} }
           whileHover={{ scale: 1.1 }}
