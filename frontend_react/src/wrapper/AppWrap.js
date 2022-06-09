b478edf3927f6ebb0c51af7fa91bd98746e6f004
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-06-09 23:11:21 +0200

    Update AppWrap.js

--- frontend_react/src/wrapper/AppWrap.js
+++ frontend_react/src/wrapper/AppWrap.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { NavigationDots, SocialMedia } from '../components';
+import { NavigationDots, SocialMedia } from '../components/Navbar';
 const AppWrap = (Component, IdName, classNames) => function HOC() {
 
   return (
 
-    <div id={idName} className={`app__container ${classNames}`}>
+    <div id={IdName} className={`app__container ${classNames}`}>
      <SocialMedia />
 
      <div className="app__wrapper app__flex">
@@ -13,10 +13,9 @@ const AppWrap = (Component, IdName, classNames) => function HOC() {
         <div className="copyright">
         <p className="p-text"><em>@2022</em> <a href="https://github.com/LawrenceBaatjies">Lawrence</a></p>
         <p className="p-text"><em>All right reserved.</em></p>
-
-        
       </div>
      </div>
+     <NavigationDots active={IdName} />
     </div>
   )
 };
