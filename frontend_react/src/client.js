7598c770d727c750cf1d57a618c8f0f5d30cea4c
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-06-09 21:21:45 +0200

    Update client.js

--- frontend_react/src/client.js
+++ frontend_react/src/client.js
@@ -1,6 +1,7 @@
 import sanityClient from '@sanity/client';
 import imageUrlBuilder from '@sanity/image-url';
 
+
 export const client = sanityClient({
 
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
@@ -11,10 +12,8 @@ export const client = sanityClient({
 });
 
 
-const Builder = imageUrlBuilder(client);
-
-
-
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
 
 
 
