e73e5cb020d1a1d1c473d6f16064165785182ff4
Lawrence Baatjies <lawrencebaatjies47@gmail.com>
2022-06-09 23:10:46 +0200

    Update Header.jsx

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { motion } from 'framer-motion';
-
-import  { images } from '../../constants';
+import { motion } from 'framer-motion'; 
+import { AppWrap } from '../../wrapper';
+import { images } from '../../constants';
 import './Header.scss';
 
 const scaleVariants = { 
@@ -16,7 +16,7 @@ const scaleVariants = {
 }
 const Header = () => {
   return (
-    <div id="home" className="app__header app__flex">
+    <div className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0,1]}}
         transition={{ duration: 3.20 }}
@@ -70,4 +70,4 @@ const Header = () => {
   )
 }
 
-export default Header;
+export default AppWrap(Header, 'home');
